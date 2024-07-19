80c3178434aab945918905240935fb1b7265c940
ramo-dev <annuar.dev@gmail.com>
2024-07-19 17:45:41 +0300

    data fetchin on DoMContentLoad

---
+++ fetch-data.js
@@ -0,0 +1,22 @@
+const dataContainer = document.getElementById("api-data");
+const userList = document.createElement("ul");
+
+dataContainer.innerHTML = "";
+
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    users.forEach((user) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = user.name;
+        userList.appendChild(listItem);
+      });
+    dataContainer.appendChild( userList)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
